3577f850495e4ce1b5bdb74b91c4a729ea8d8f0b
Sristi1123 <144992720+Sristi1123@users.noreply.github.com>
2026-03-20 22:02:47 +0530

    Time: 8 ms (5.22%), Space: 12.2 MB (7.74%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    bool check(stack<char> &st, string &part, int m){
-        stack<char> s=st;
-        for(int i=m-1;i>=0;i--){
-            if(s.top()!=part[i]){
-                return false;
-            }
-            s.pop();
-        }
-        return true;
-    }
+    // bool check(stack<char> &st, string &part, int m){
+    //     stack<char> s=st;
+    //     for(int i=m-1;i>=0;i--){
+    //         if(s.top()!=part[i]){
+    //             return false;
+    //         }
+    //         s.pop();
+    //     }
+    //     return true;
+    // }
     string removeOccurrences(string s, string part) {
         // --> Approach 1
         // while(true){
@@ -22,24 +22,36 @@ public:
         // return s;
 
         // --> Approach 2
-        stack<char> st;
-        int n=s.length();
-        int m=part.length();
-        for(int i=0;i<n;i++){
-            st.push(s[i]);
+        // stack<char> st;
+        // int n=s.length();
+        // int m=part.length();
+        // for(int i=0;i<n;i++){
+        //     st.push(s[i]);
 
-            if(st.size()>=m && check(st,part,m)==true){
-                for(int j=0;j<m;j++){
-                    st.pop();
-                }
+        //     if(st.size()>=m && check(st,part,m)==true){
+        //         for(int j=0;j<m;j++){
+        //             st.pop();
+        //         }
+        //     }
+        // }
+        // string res="";
+        // while(!st.empty()){
+        //     res.push_back(st.top());
+        //     st.pop();
+        // }
+        // reverse(res.begin(),res.end());
+        // return res;
+
+        // --> Approach 3
+        string res="";
+        int n=part.length();
+        for(char &c: s){
+            res.push_back(c);
+
+            if(res.length()>=n && res.substr(res.length()-n)==part){
+                res.erase(res.length()-n);
             }
         }
-        string res="";
-        while(!st.empty()){
-            res.push_back(st.top());
-            st.pop();
-        }
-        reverse(res.begin(),res.end());
         return res;
     }
 };
