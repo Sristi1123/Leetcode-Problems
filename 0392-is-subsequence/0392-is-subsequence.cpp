5cf9264bd996c66147ed9fcb464ff7996895d293
Sristi1123 <144992720+Sristi1123@users.noreply.github.com>
2026-04-07 09:55:51 +0530

    Time: 0 ms (100%), Space: 13.9 MB (5.16%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int t[101][10001];
+    int lcs(string &s, string &u, int n, int m){
+        if(n==0||m==0) return 0;
+        if(t[n][m]!=-1) return t[n][m];
+        int res=0;
+        if(s[n-1]==u[m-1]){
+            t[n][m] = 1 + lcs(s, u, n-1, m-1);
+        }else{
+            t[n][m]=max((lcs(s, u, n-1,m)),(lcs(s, u, n, m-1)));
+        }
+        return t[n][m];
+    }
+    bool isSubsequence(string &s, string &u) {
+        int n=s.length();
+        int m=u.length();
+        memset(t,-1,sizeof(t));
+        int a=lcs(s,u,n,m);
+        return a==n;
+    }
+};
