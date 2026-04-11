2e767a706b14b61d9391e218a431c20941e4f671
Sristi1123 <144992720+Sristi1123@users.noreply.github.com>
2026-04-11 19:01:28 +0530

    Time: 37 ms (63.33%), Space: 57.8 MB (47.35%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    bool isPalindrome(string &s, int i, int j){
-        while(i<=j){
-            if(s[i]!=s[j]){
-                return false;
-            }
-            i++;
-            j--;
+    int p[1001][1001];
+    bool isPalindrome(string &s, int i, int j) {
+        if (p[i][j] != -1) return p[i][j]; // already computed!
+        int l = i, r = j;
+        while (l <= r) {
+            if (s[l] != s[r]) return p[i][j] = 0;
+            l++; r--;
         }
-        return true;
+        return p[i][j] = 1;
     }
     void solve(string &s, int i, vector<vector<string>> &ans,vector<string> &path){
         if(i == s.length()){
@@ -26,6 +26,7 @@ public:
     }
     vector<vector<string>> partition(string s) {
         int n=s.length();
+        memset(p, -1, sizeof(p));
         vector<vector<string>> ans;
         vector<string> path;
         solve(s,0,ans, path);
