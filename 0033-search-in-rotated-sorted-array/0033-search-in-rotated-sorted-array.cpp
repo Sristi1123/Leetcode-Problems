7499f8b02b5d877ba346ccab4e2b74987d27d0ed
Sristi1123 <144992720+Sristi1123@users.noreply.github.com>
2026-06-24 12:00:33 +0530

    Time: 0 ms (100%), Space: 15.1 MB (67.13%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -6,15 +6,17 @@ public:
         int end=n-1;
         while(start<=end){
             int mid=start+(end-start)/2;
-            if(nums[mid]==target) return mid;
+            if(nums[mid]==target){
+                return mid;
+            }
             if(nums[start]<=nums[mid]){
-                if(nums[start]<=target && target<nums[mid]){
+                if(target>=nums[start] && target<nums[mid]){
                     end=mid-1;
                 }else{
                     start=mid+1;
                 }
             }else{
-                if(nums[mid]<target && target<=nums[end]){
+                if(target>nums[mid] && target<=nums[end]){
                     start=mid+1;
                 }else{
                     end=mid-1;
