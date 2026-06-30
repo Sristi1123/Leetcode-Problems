12c38b4365707eeeaa417fb3384d6b0e3e7b3b7d
Sristi1123 <144992720+Sristi1123@users.noreply.github.com>
2026-06-30 19:42:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n=s.size();
+        stack<char> st;
+        for(auto ch: s){
+            if(ch=='(' || ch=='{' || ch=='['){
+                st.push(ch);
+            }else{
+                if(st.empty()) return false;
+                char a=st.top();
+                if(a=='(' && ch!=')' || a=='{' && ch!='}' || a=='[' && ch!=']') return false;
+                st.pop();
+            }
+        }
+        return true;
+    }
+};
