84919dfe424ce4835282738170a1d1abce71ae13
Sristi1123 <144992720+Sristi1123@users.noreply.github.com>
2026-08-14 10:57:32 +0530

    Time: 96 ms (72.47%), Space: 21.7 MB (52.61%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int t[201][1001];
+    int t[201][10001];
     bool check(vector<int>& nums, int sum, int n){
         if(n==0 && sum==0) return true;
         for(int i=0;i<n+1;i++){
@@ -12,9 +12,9 @@ public:
         for(int i=1;i<n+1;i++){
             for(int j=1;j<sum+1;j++){
                 if(nums[i-1]<=j){
-                    t[n][j]=t[i-1][j-nums[i-1]] ||t[i-1][j];
+                    t[i][j]=t[i-1][j-nums[i-1]] ||t[i-1][j];
                 }else{
-                    t[n][j]=t[i-1][j];
+                    t[i][j]=t[i-1][j];
                 }
             }
         }
