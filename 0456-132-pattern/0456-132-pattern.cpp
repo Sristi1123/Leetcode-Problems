5ebda776e0556546d5233c31e24b1aaefa2c039c
Sristi1123 <144992720+Sristi1123@users.noreply.github.com>
2026-09-02 09:21:26 +0530

    Time: 7 ms (91.53%), Space: 70.6 MB (72.55%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.cpp
+++ 0456-132-pattern/0456-132-pattern.cpp
@@ -3,16 +3,16 @@ public:
     bool find132pattern(vector<int>& nums) {
         int n=nums.size();
         stack<int> st;
-        int x=INT_MIN;
+        int x=INT_MIN;     // it stores the second largest value
         for(int i=n-1;i>=0;i--){
             if(nums[i]<x){
-                return true;
+                return true;        // if nums[i] is the smallest from the 2nd largest then true
             }
             while(!st.empty() && st.top()<nums[i]){
                 x=st.top();
                 st.pop();
             }
-            st.push(nums[i]);
+            st.push(nums[i]);    //st.top() is the largest element 
         }
         return false;
     }
